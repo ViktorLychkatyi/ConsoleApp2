cde6cee5bb1b18dddfc4eee3ae1c2b47f1e81caf
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2025-01-03 19:58:43 +0100

    first task

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace ConsoleApp2
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test homework");
+            int width = 6;
+            int height = 5;
+            int[,] ar = new int[height, width];
+            int number = 1;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    ar[y, x] = number++;
+                    Console.Write("{0, -8}", ar[y, x]); // -5 это для выравнивание по левому краю в ряд
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
